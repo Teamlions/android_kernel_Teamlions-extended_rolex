46ebe79b2b5db2c70bdc9d165247470522d5d991
muralivijay <muralidharan9845@gmail.com>
2019-03-09 17:44:07 +0530

    build-script: show our shell output colorful
    * Some text corrections
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- build.sh
+++ build.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 # Author- Muralivijay, bitrvmpd
+
+# Set Message Color Variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+WHITE='\033[1m'
+NC='\033[0m'
+
+# Clear Screen before staring our script
 clear
-echo "#######################################################"
-echo "##### Teamlions-Extended Kernel - Build Script ########"
-echo "#######################################################"
+# Heading
+echo -e "${WHITE}#######################################################${NC}"
+echo -e "${WHITE}##### Teamlions-Extended Kernel - Build Script ########${NC}"
+echo -e "${WHITE}#######################################################${NC}"
 
 # Any2kernel2
-echo "Clone Any2kernel2 if you don,t have "
+echo -e "${GREEN}Clone Any2kernel2 if you don,t have${NC}"
 git clone https://github.com/muralivijay/AnyKernel2.git -b 9.0-teamlions-rolex ~/AnyKernel2_teamlions/
 
 # Toolchain
-echo "Clone Linaro arm64 toolchain if you don,t have "
+echo -e "${GREEN}Clone Linaro arm64 toolchain if you don,t have${NC} "
 git clone https://github.com/Teamlions/aarch64-linux-gnu-7.3.git ~/aarch64-linux-gnu-7.3
 
 # Make clean build
-echo "${blu}Make clean build?${txtrst}"
+echo -e "${blu}${BLUE}Make clean build?${NC}${txtrst}"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) make clean && make mrproper; break;;
@@ -23,15 +33,16 @@ select yn in "Yes" "No"; do
 done
 
 # Make Sure Cleanup Any2kernel dir build
-echo " Check your Home dir Any2kernel_teamlions Cloned           "
-echo " Make sure run cleanup.sh script if you are clean building "
-echo " Else when Compress zip it will become Dirty pack Remember "
-echo " Do it first                                               "
-echo "${blu} Are You Done this step ?${txtrst}"
+echo -e "${GREEN} Check your Home dir Any2kernel_teamlions Cloned ${NC}           "
+echo -e "${GREEN} Make sure run cleanup.sh script if you are clean building ${NC} "
+echo -e "${GREEN} Else when Compress zip it will become Dirty pack Remember ${NC} "
+echo -e "${RED} Do it first ${NC}"
+echo -e "${BLUE} Or ==>experiment<== you can build kernel with dirty but any2kernel_teamlions must be run clean step before doing this ${NC}"
+echo -e "${blu}${BLUE} Are You Done this step ?${NC}${txtrst}"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) echo " Good You can Continue to build" ; break;;
-        No ) echo " You must run Clean step before building kernel exiting " && exit ; break;;
+        Yes ) echo -e "${GREEN}Good You can Continue to build${NC}" ; break;;
+        No ) echo -e "${RED}You must run Clean step before building kernel. now stoping build${NC} " && exit ; break;;
     esac
 done
 
@@ -79,20 +90,20 @@ then
 fi
 
 # Export ARCH-arm64 & Launch rolex
-echo " Config Kernel if you want add or disable any driver or feature "
+echo -e "${GREEN} Config Kernel if you want add or disable any driver or feature${NC} "
 echo "                                                                "
 export ARCH=arm64 && export SUBARCH=arm64 && make rolex_defconfig && make menuconfig
 
 # Build Process
-echo -e "> Opening rolex_config file...\n"
-echo -e "> Starting kernel compilation using rolex_defconfig file...\n"
+echo -e "${GREEN}> Opening rolex_config file...\n${NC}"
+echo -e ">${GREEN} Starting kernel compilation using${NC} ${BLUE}rolex_defconfig${NC} ${GREEN}file...\n${NC}"
   CROSS_COMPILE=$CROSS_COMPILE make -j$( nproc --all )
 
 start=$SECONDS
 
 # Get current kernel version
 KERNEL_VERSION=$(head -n3 Makefile | sed -E 's/.*(^\w+\s[=]\s)//g' | xargs | sed -E 's/(\s)/./g')
-echo -e "\n\n> Packing Teamlions-Extended Kernel v$KERNEL_VERSION\n\n"
+echo -e "\n\n> ${GREEN}Packing Teamlions-Extended Kernel v$KERNEL_VERSION\n\n${NC}"
 # Pack the kernel as a flashable TWRP zip. Pie Edition
 ~/AnyKernel2_teamlions/build.sh $KERNEL_VERSION PIE
 
